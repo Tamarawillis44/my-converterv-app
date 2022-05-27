b9c10c96d585d3219a3e33b06a8bb6eaba124c26
Tamarawillis44 <techietl444@gmail.com>
2022-05-26 23:13:06 -0400

    Initial Commit

---
+++ Chef Kitchen Converter/Chef_Kitchen_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chef_Kitchen_ConverterApp.swift
+//  Chef Kitchen Converter
+//
+//  Created by Tamara laraque on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Chef_Kitchen_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chef Kitchen Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chef Kitchen Converter
+//
+//  Created by Tamara laraque on 5/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
